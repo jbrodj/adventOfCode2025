14da4c0750a35a9248631d85f5a06cf5b6344820
J Brodie Day <jbrodieday@gmail.com>
2026-01-21 10:58:34 -0500

    001: add pseudocode

---
+++ 01/01.js
@@ -0,0 +1,22 @@
+// Given a sequence of instructions to modify a number 0 thru 99 (which runs through -- 99 + 1 == 0; 0 - 1 == 99), find the count of instances where the number is exactly zero after an instruction.
+
+// Import the list of instructions
+// -- Read the file
+// -- Create array of strings
+
+
+// Function to parse an instruction -- 
+// -- Read the direction
+// -- Read the number
+// -- Update the dial position
+
+
+// Initialize dial position variable
+// Initialize zero count variable
+
+
+// Loop over the list
+// -- Update the dial position
+// -- Check condition: is number === 0
+
+// Print final zero count var value once loop is complete
