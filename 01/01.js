07a40d50d7838bad6ebfc899a08641a39f6200d0
J Brodie Day <jbrodieday@gmail.com>
2026-01-22 11:51:16 -0500

    001: refactor and cleanup

--- 01/01.js
+++ 01/01.js
@@ -4,40 +4,39 @@
 // Import the list of instructions
 // -- Read the file using node fs and return string
 const fs = require('fs')
-const filePath = './problem01Inputs.txt'
+const inputFilePath = './problem01Inputs.txt'
 const readFileFromSrc = (path) => {
   try {
-    const file = fs.readFileSync(filePath)
+    const file = fs.readFileSync(path)
     return file.toString()
   } catch (error) {
     console.error('Error reading file: ', error.message)
   }
 }
-const data = readFileFromSrc()
+const instructionsStr = readFileFromSrc(inputFilePath)
 
 // -- Create array of strings by splitting str by new line
-const instructionsList = data.split(`\n`)
-
+const instructionsList = instructionsStr.split(`\n`)
 
 // Helper functions to parse an instruction -- 
-// -- Read the direction of dial turn
+// -- Read the direction of dial turn by checking first char
 const isRightTurn = (instruction) => {
   return instruction[0].toUpperCase() === 'R' ? true : false
 }
 // -- Read the instruction number -- accounting for numbers larger than 99
 const getInteger = (instruction) => {
-  let num = parseInt(instruction.slice(1))
-  return num > 99 ? num % 100 : num
+  // Get number from current instruction string by removing first char; 
+  // Some instructions are > 99, so we modulo by 100 to convert to relative dial position change
+  let num = parseInt(instruction.slice(1)) % 100
+  // Convert left turn instructions to negative ints
+  if (!isRightTurn(instruction)) { num = 0 - num}
+  return num
 }
-// -- Update the dial position
+// -- Update the dial position -- accounting for sums greater than 99
 const updateDialPosition = (instruction) => {
-  const rightTurn = isRightTurn(instruction)
-  dialPosition = rightTurn ? 
-    (dialPosition + getInteger(instruction)) % 100 : 
-    (dialPosition - getInteger(instruction)) % 100
+  dialPosition = (dialPosition + getInteger(instruction)) % 100
   // Account for negative dial position by translating to corresponding positive integer
   dialPosition = dialPosition < 0 ? dialPosition = dialPosition + 100 : dialPosition
-  console.log('Current dial position: ', dialPosition)
 }
 // -- Check current value and conditionally increment zero count value
 const checkIncrementZeroCount = () => {
@@ -46,13 +45,12 @@ const checkIncrementZeroCount = () => {
   }
 }
 
-
 // Initialize dial position variable
 let dialPosition = 50
 // Initialize zero count variable
 let zeroCount = 0
 
-// Loop over the list
+// Loop over the instructions list and execute instructions
 for (index in instructionsList) {
   updateDialPosition(instructionsList[index])
   checkIncrementZeroCount()
