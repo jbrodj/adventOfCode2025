07260c8d4167a0638e90e88e7939d51513c5b02b
J Brodie Day <jbrodieday@gmail.com>
2026-01-23 10:41:10 -0500

    003: add pseudocode

---
+++ 03/03.js
@@ -0,0 +1,21 @@
+// ==== Advent of Code problem 3 ====
+
+// An invalid ID is one that consists of two identical sequences repeated (ie. 55, 2020, 123123). 
+  // -- Only ints with even num of digits can be invalid -- we can split them at their middle index and check for equality on both sides.
+// Ranges to be checked are defined in the input. 
+// We require the sum of all invalid IDs, so we need to either store them or track a rolling sum. 
+
+// Load input file
+// Create list of ranges
+
+// Helper functions
+// -- Check for invalid ID 
+    // Convert to string and split
+    // Compare first half and second half of num
+// -- Add current invalid ID to sum of all invalid IDs
+
+// Initialize invalid ID sum
+
+// Run loop over list of ranges
+
+// Print sum of all invalid IDs
