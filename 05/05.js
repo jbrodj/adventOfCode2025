48c869a3079eb65eabd110cc2004e3deb4a23c9d
J Brodie Day <jbrodieday@gmail.com>
2026-01-27 11:22:33 -0500

    005: refactor and cleanup

--- 05/05.js
+++ 05/05.js
@@ -1,6 +1,6 @@
 // ==== Advent of Code problem 5 ====
 
-// Given a string of numbers, find the largest possible two-digit integer that can be composed by combining two
+// Given a string (bank) of numbers, find the largest possible two-digit integer that can be composed by combining two
 // digits from that string -- the numbers must remain in order (ie. the second digit must be to the right of the first digit)
 // We will want to use the greatest possible number for the tens column of our integer, and find whatever is the largest remaining
 // digit to the right of that to compose the greatest possible int.
@@ -8,7 +8,7 @@
 // This also means if there are multiple of the same greatest digit, if reading from left to right, we'll want to use the first instance
 // to optimize the choice of next largest numbers to the right of it. 
 // Since strings are iterable, we'll want to leave the row in its string form to allow looping over the string to find the greatest value. 
-// Do this for each row in a list of strings, and sum all resulting integers. 
+// Do this for each row in a list of banks, and sum all resulting integers to find total max value (joltage value). 
 
 // Import and read input file using node fs and return string
 const fs = require('fs')
@@ -25,47 +25,33 @@ const numericBanksStr = readFileFromSrc(inputFilePath)
 
 // Initialize list of numeric banks from input string
 const numericBanksList = numericBanksStr.split('\n')
-const mockBanksList = [
-  '987654321111111', 
-  '811111111111119', 
-  '234234234234278', 
-  '818181911112111'
-]
+
 // Helper functions
-// -- Find greatest numeric value in a given string
+// -- Find greatest numeric value in a given string bank
 const findGreatestNumber = (bank, isTensDigit) => {
   // Tens digit cannot be last digit in bank (elim last digit from loop if tens digit)
   const loopLen = isTensDigit ? bank.length - 1 : bank.length
-
-  // Loop over input string
   let highestNum = '0'
   for (let i = 0; i < loopLen; i++) {
-    // Track current highest number by comparing each char
+    // Track current highest number by comparing each char to prev highest found
     highestNum = bank[i] > highestNum ? bank[i] : highestNum
   }
-  // console.log(highestNum)
   return highestNum
 }
-// -- Add highest integer to rolling sum
-  // Coerce two digit string to int by adding to initialized int variable
-
 // Initialize sum variable
 let joltageSum = 0
 
 // Loop over list of input strings
 for (row in numericBanksList) {
   const currentBank = numericBanksList[row]
+  // Get num for tens digit
   const tensDigit = findGreatestNumber(currentBank, true)
+  // Get num for ones digit from substring of chars to right of tens digit num
   const bankSubtr = currentBank.slice(currentBank.indexOf(tensDigit) + 1)
   const onesDigit = findGreatestNumber(bankSubtr)
-  const twoDigitNum = tensDigit+onesDigit
-
-  console.log('current bank: ', currentBank)
-  console.log('tens digit:', tensDigit)
-  console.log('find index: ', currentBank.indexOf(tensDigit))
-  console.log('substring for second digit: ', bankSubtr)
-  console.log('Two digit num: ', twoDigitNum)
+  // Compose two-digit num str and convert to int
   maxJoltageInBank = parseInt(tensDigit + onesDigit)
+  // Add integer to rolling sum
   joltageSum = joltageSum + maxJoltageInBank
 }
 
