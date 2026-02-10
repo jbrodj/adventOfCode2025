ec9f49367a675d05ec3d637fe09d8cdea1864b3e
J Brodie Day <jbrodieday@gmail.com>
2026-02-10 12:26:23 -0500

    010: add logic

--- 10/10.js
+++ 10/10.js
@@ -11,3 +11,51 @@
 // Instead, let's loop through our subset of n and count the difference between the upper limit and lower limit of 
 // each range (inclusive), making sure to account for overlapping ranges. We'll have to sort the input array, which adds
 // time complexity. 
+
+const fs = require('fs')
+const inputFilePath = '../09/09-input.txt'
+const readFileFromSrc = (path) => {
+  try {
+    const file = fs.readFileSync(path)
+    return file.toString()
+  } catch (error) {
+    console.error('Error reading file: ', error.message)
+  }
+}
+// Create array from input string
+const inputArr = readFileFromSrc(inputFilePath).split('\n')
+
+// Split range strings into sorted 2d-array (numerically by the first index of each sub-array) -- sorting makes it simpler to find overlaps
+// -- We only need the first part of the input array that contains the ranges
+const getSortedRangesArray = (inputArr) => {
+  let rangesArr = []
+  for (index in inputArr) {
+    // End loop when we reach the end of the ranges (ie. at the empty new line)
+    if (index > 0 && inputArr[index] === '') {break}
+    rangesArr.push(inputArr[index].split('-'))
+  }
+  return rangesArr.sort((a, b) => a[0]-b[0])
+}
+
+const getFreshIngredientCount = (sortedArr) => {
+  let freshIngredientCount = 0
+  for (index in sortedArr) {
+    // Check previous range for overlap with current range (null prevRange for index 0)
+    const prevRange = index > 0 ? sortedArr[parseInt(index) - 1] : null
+    let currentRange = sortedArr[index]
+    // Adjust lower limit of current range if current range overlaps with previous range (convert to ints to avoid
+    // comparison & arithmetic issues with numeric strings)
+    if (prevRange && parseInt(prevRange[1]) >= parseInt(currentRange[0])) {
+      currentRange[0] = parseInt(prevRange[1]) + 1
+    }
+    // Count indecies in current (or adjusted) range (inclusive of range boundary nums)
+    const numOfIndeciesInRange = currentRange[1] - currentRange[0] + 1
+    // In cases where numOfIndeciesInRange is negative, the current range is a subset of the prev range with no new indecies
+    // and can be discarded
+    freshIngredientCount = numOfIndeciesInRange > 0 ? freshIngredientCount + numOfIndeciesInRange : freshIngredientCount
+  }
+
+  return freshIngredientCount
+}
+
+console.log('Total count: ', getFreshIngredientCount(getSortedRangesArray(inputArr)))
