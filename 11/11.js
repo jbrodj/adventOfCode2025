7081d97aa609b70334841ebd405f43946f1b10fb
J Brodie Day <jbrodieday@gmail.com>
2026-02-11 12:41:28 -0500

    011: add logic

--- 11/11.js
+++ 11/11.js
@@ -13,8 +13,63 @@
 // We can check the operator of that given column by looking at arr.length index of the given column, and exclude that last
 // row from the arithmetic loop 
 // Since we're performing arithmetic operations on these numeric strings, we'll need to convert them to ints.
+// The text input also has extra spaces in it (each num isn't necessarily separated by only one space), so we'll need to 
+// eliminate the empty strings from our 2d array. 
 
-const mockInput = `123 328  51 64 
- 45 64  387 23 
-  6 98  215 314
-*   +   *   +  `
+// Import input text (trim trailing new line)
+const fs = require('fs')
+const inputFilePath = './11-input.txt'
+const readFileFromSrc = (path) => {
+  try {
+    const file = fs.readFileSync(path)
+    return file.toString()
+  } catch (error) {
+    console.error('Error reading file: ', error.message)
+  }
+}
+const inputTxt = readFileFromSrc(inputFilePath).trim()
+
+// Convert str input to 2d array
+const composeNestedArray = (input) => {
+  // Break string into 1d arr by line
+  let nestedArray = input.split('\n')
+  // Break each line into array by empty space
+  for (row in nestedArray) {
+    nestedArray[row] = nestedArray[row].split(' ')
+  }
+  // Rm empty strings from each row
+  for (row in nestedArray) {
+    nestedArray[row] = nestedArray[row].filter((value) => {
+      return value !== ''
+    })
+  }
+  return nestedArray
+}
+
+const getGrandTotal = (inputTxt) => {
+  const nestedOperationArray = composeNestedArray(inputTxt)
+  // The num of operations will be equal to how many columns are in a line of the data.
+  const numOfColumns = nestedOperationArray[0].length
+  // Num of numbers in each operation will be equal to the number of rows minus 1 (the operator row at bottom)
+  const numOfRows = nestedOperationArray.length - 1
+  let grandTotal = 0
+  // Nested loop to perform operations on each column
+  for (let col = 0; col < numOfColumns; col++) {
+    let rollingColSum = 0
+    for (let row = 0; row < numOfRows; row++) {
+      const currentNum = parseInt(nestedOperationArray[row][col])
+      const operation = nestedOperationArray[numOfRows][col]
+      // For first element, set rollingColSum = that value and move to next
+      if (!rollingColSum) {
+        rollingColSum = currentNum
+        continue
+      }
+      if (operation === '+') {rollingColSum += currentNum}
+      if (operation === '*') {rollingColSum *= currentNum}
+      // rollingColSum = operation === '+' ? rollingColSum+= currentNum : rollingColSum * currentNum
+    }
+    grandTotal += rollingColSum
+  }
+  return grandTotal
+}
+console.log('Grand total: ', getGrandTotal(inputTxt))
