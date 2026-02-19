fa33f74813fb2675155f3ba5e93b003b2aa03972
J Brodie Day <jbrodieday@gmail.com>
2026-02-19 12:17:08 -0500

    013: add logic and input file

--- 13/13.js
+++ 13/13.js
@@ -19,23 +19,73 @@
 // We can use the string index within each line to track the lateral positions
 // We'll keep a counter for splits and print it.
 
+const fs = require('fs')
+const { get } = require('http')
+const inputFilePath = './13-input.txt'
+const readFileFromSrc = (path) => {
+  try {
+    const file = fs.readFileSync(path)
+    return file.toString()
+  } catch (error) {
+    console.error('Error reading file: ', error.message)
+  }
+}
+// Split our input into array by line
+const manifoldArray = readFileFromSrc(inputFilePath).split('\n')
 
+// Parse a line in the manifold array and return the modified next line
+const runLine = (manifoldArray, lineIndex) => {
+  const nextLineIndex = parseInt(lineIndex) + 1
+  // If end of array, break
+  if (!manifoldArray[nextLineIndex]) {
+    return
+  }
+  const currentLine = manifoldArray[lineIndex]
+  let updatedNextLine = manifoldArray[nextLineIndex]
 
-const mockInput = `
-.......S.......
-...............
-.......^.......
-...............
-......^.^......
-...............
-.....^.^.^.....
-...............
-....^.^...^....
-...............
-...^.^...^.^...
-...............
-..^...^.....^..
-...............
-.^.^.^.^.^...^.
-...............
-`.trim()
+  for (char in currentLine) {
+    const currentIndex = parseInt(char)
+    const currentChar = currentLine[char]
+    // If char is a tachyon
+    if (currentChar === 'S' || currentChar === '|') {
+    // Send tachyon to line below:
+      // If char at next line is a splitter
+      if (manifoldArray[nextLineIndex][char] === '^') {
+        const leftSplitIndex = currentIndex - 1
+        const rightSplitIndex = currentIndex + 1
+        // Check if either of the next line char at the split indecies are already tachyons, if not, increment split count
+        if (updatedNextLine[leftSplitIndex] !== '|' || updatedNextLine[rightSplitIndex] !== '|') {
+          splitCount++
+          // Add tachyon chars to next line
+          updatedNextLine = 
+            updatedNextLine.slice(0, leftSplitIndex) + 
+            '|' + 
+            updatedNextLine.slice(leftSplitIndex + 1, rightSplitIndex) + 
+            '|' + 
+            updatedNextLine.slice(rightSplitIndex + 1)
+        }
+      }
+      // If char at next line is empty space
+      else {
+        // Add tachyon char to next line
+        updatedNextLine = 
+          updatedNextLine.slice(0, char) + 
+          '|' + 
+          updatedNextLine.slice(parseInt(char) + 1)
+      }
+    }
+  }
+  return updatedNextLine
+}
+
+let splitCount = 0
+const getSplitCount = (manifoldArray) => {
+  // For each line in the manifold, update the next line with positions of tachyons from current line
+  for (line in manifoldArray) {
+    const nextLineIndex = parseInt(line) + 1
+    const nextLine = runLine(manifoldArray, line)
+    manifoldArray[nextLineIndex] = nextLine ? nextLine : manifoldArray[nextLineIndex]
+  }
+  return splitCount
+}
+console.log('Final split count: ', getSplitCount(manifoldArray))
