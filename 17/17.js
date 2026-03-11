d7cd7bde66327873e079a0be622266fdc7f12881
J Brodie Day <jbrodieday@gmail.com>
2026-03-11 11:46:02 -0400

    017: add logic

--- 17/17.js
+++ 17/17.js
@@ -11,3 +11,41 @@
 // Then nested loop over our red tiles, calculating the size of the rectangle transcribed by the current tile and each other tile
 // -- reducing the size of the inner loop each time because we've already calculated 
 // We can track the largest area so far and replace that value if we find a larger one, returning the largest area at the end
+
+const fs = require('fs')
+const inputFilePath = './17-input.txt'
+const readFileFromSrc = (input) => {
+  try {
+    const file = fs.readFileSync(input)
+    return file.toString()
+  } catch (error) {
+    console.error('Error reading file: ', error.message)
+  }
+}
+// Split our input into a list of arrays for easy indexing of each coordinate
+const inputStr = readFileFromSrc(inputFilePath).trim().split('\n')
+const coordinateArray = inputStr.map((index) => index.split(','))
+
+// Given two sets of x-y coordinates, as [x, y], return area of a rectangle transcribing those points
+const findArea = (coord1, coord2) => {
+  const x = Math.abs(coord1[0] - coord2[0] + 1)
+  const y = Math.abs(coord1[1] - coord2[1] + 1)
+  // console.log(x, y, x * y)
+  return x * y
+}
+
+// For each coordinate in a given array, compare area against largest area found so far (reducing size of inner loop each iteration to avoid duplcation)
+const findLargestArea = (coordinateArray) => {
+  let largestArea = 0
+  const len = coordinateArray.length
+  for (coordinateIndex in coordinateArray) {
+    for (let i = parseInt(coordinateIndex) + 1; i < len; i++) {
+      const currentArea = findArea(coordinateArray[coordinateIndex], coordinateArray[i])
+      // console.log(coordinateArray[coordinateIndex], '|', coordinateArray[i], '|', currentArea)
+      largestArea = currentArea > largestArea ? currentArea : largestArea
+    }
+  }
+  return largestArea
+}
+
+console.log(findLargestArea(coordinateArray))
